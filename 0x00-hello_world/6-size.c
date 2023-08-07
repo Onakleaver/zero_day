33c2cde610abfc10d0cf73638f00cfb354d3945d
Kleaver <KLeaver49@gmail.com>
2023-08-07 20:49:51 +0300

    Utilizes ``puts`` function instead of printf

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,11 @@ int main(void)
 	long long d;
 	float e;
 
-	printf("Size of a char: %lu bytes(s)\n", sizeof(a));
-	printf("Size of an int: %lu bytes(s)\n", sizeof(b));
-	printf("Size of a long int: %lu bytes(s)\n", sizeof(c));
-	printf("Size of a long long: %lu bytes(s)\n", sizeof(d));
-	printf("Size of a float: %lu bytes(s)\n", sizeof(e));
+	puts("Size of a char: %lu bytes(s)", sizeof(a));
+	puts("Size of an int: %lu bytes(s)", sizeof(b));
+	puts("Size of a long int: %lu bytes(s)", sizeof(c));
+	puts("Size of a long long: %lu bytes(s)", sizeof(d));
+	puts("Size of a float: %lu bytes(s)", sizeof(e));
+
 	return (0);
 }
