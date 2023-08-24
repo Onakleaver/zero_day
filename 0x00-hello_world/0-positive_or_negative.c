260f97749fccbdab52020e47d9a4a6c25faa34a5
Kleaver <KLeaver49@gmail.com>
2023-08-24 05:06:44 +0300

    j

---
+++ 0x00-hello_world/0-positive_or_negative.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main -Checks if the value of n is positive or negative.
+ *
+ * Return: Always (0)
+ */
+int main(void)
+{
+
+}
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,11 @@
 /**
  * main- Entry point
  *
- * Return: ALways 0
+ * Return: Always 0
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
+
 }
