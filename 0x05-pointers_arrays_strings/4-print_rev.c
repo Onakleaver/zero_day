f1606a42d4273d5a3491b853b4beb136e40e51be
Kleaver <KLeaver49@gmail.com>
2023-09-27 06:40:59 +0300

    Prints string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * print_rev -prints strin in reverse
+  * @s: pointer to string
+  * Return: void
+  */
+void print_rev(char *s)
+{
+	for (*s == '\0', i )
+	{
+		s--;
+		_putchar(*s);
+	}
+	_putchar('\n');
+	
+}
